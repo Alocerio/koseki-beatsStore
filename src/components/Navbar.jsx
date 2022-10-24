46584229228396f5421df6c4ca2fb16648c0ba36
Leandro Bordon <leandrobordon@Leandros-MacBook-Air.local>
2022-10-23 23:21:58 -0300

    slogan up-date

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
         <div className="p-5 text-center  rounded-xl px-10   ">
           <h3 class="text-xl italic text-white font-normal drop-shadow-2xl  mb-8 ">
-            "Your next hit music could be starts from here"
+            "Your next hit music could be starting from here"
           </h3>
           <a
             class="inline-block px-10 py-2.5 bg-white hover:text-white hover:bg-secondary text-bg font-medium text-md leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
